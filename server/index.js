90efddc3c79209c0778abd16a1cbee0dfe21b6bc
Maximilian Grundke <maximilian.grundke@student.hpi.uni-potsdam.de>
2015-07-03 13:44:10 +0200

    Modify server to accept boundingRect as input

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var bbb_require = require("babelsbergjs-require");
 
 var CollisionConstraint = bbb_require('./server/constraint_avoids_collision.bbb');
+
 var rects = {
-  'first': {x: 0, y: 0, width: 10, height: 10},
-  'second': {x: 10, y: 10, width: 10, height: 10}
+  'first': {width: 200, height: 100, x: 50, y: 50},
+  'second': {width: 200, height: 100, x: 300, y: 300}
 };
+
 var constraint = new CollisionConstraint(rects['first'], rects['second']);
 constraint.enforce();
 
@@ -13,25 +15,24 @@ var app = express();
 app.use(express.static('server/public'));
 
 app.get('/position/:rect_id', function (req, res) {
-  req.accepts(['json']);
-  rects[req.params.rect_id].x = req.query.x;
-  rects[req.params.rect_id].y = req.query.y;
-  rects[req.params.rect_id].width = req.query.width;
-  rects[req.params.rect_id].height = req.query.height;
+  rects[req.params.rect_id].x = parseInt(req.query.left);
+  rects[req.params.rect_id].y = parseInt(req.query.top);
+  rects[req.params.rect_id].width = parseInt(req.query.width);
+  rects[req.params.rect_id].height = parseInt(req.query.height);
   res.json({
-  'first': {
-    x: rects['first'].x,
-    y: rects['first'].y,
-    width: rects['first'].width,
-    height: rects['first'].height
-  },
-  'second': {
-    x: rects['second'].x,
-    y: rects['second'].y,
-    width: rects['second'].width,
-    height: rects['second'].height
-  }
-});
+    'first': {
+      x: rects['first'].x,
+      y: rects['first'].y,
+      width: rects['first'].width,
+      height: rects['first'].height
+    },
+    'second': {
+      x: rects['second'].x,
+      y: rects['second'].y,
+      width: rects['second'].width,
+      height: rects['second'].height
+    }
+  });
 });
 
 var server = app.listen(3000, function () {
