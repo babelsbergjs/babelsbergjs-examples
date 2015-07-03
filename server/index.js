b427951e6e7d3d98971ed1b3e0e724b0a249609e
FHorschig <f.horschig@googlemail.com>
2015-07-03 11:53:49 +0200

    Constraints being solved.

--- server/index.js
+++ server/index.js
@@ -1,19 +1,42 @@
-var bbb = require("babelsbergjs-core");
-var bbb_cassowary = require("babelsbergjs-cassowary");
+var express = require('express');
+var bbb_require = require("babelsbergjs-require");
 
-var express = require('express')
+var CollisionConstraint = bbb_require('./server/constraint_avoids_collision.bbb');
+var rects = {
+  'first': {x: 0, y: 0, width: 10, height: 10},
+  'second': {x: 10, y: 10, width: 10, height: 10}
+};
+var constraint = new CollisionConstraint(rects['first'], rects['second']);
+constraint.enforce();
+
 var app = express();
-
 app.use(express.static('server/public'));
 
-app.get('/positions', function (req, res) {
-  res.send('30,30');
+app.get('/position/:rect_id', function (req, res) {
+  req.accepts(['json']);
+  rects[req.params.rect_id].x = req.query.x;
+  rects[req.params.rect_id].y = req.query.y;
+  rects[req.params.rect_id].width = req.query.width;
+  rects[req.params.rect_id].height = req.query.height;
+  res.json({
+  'first': {
+    x: rects['first'].x,
+    y: rects['first'].y,
+    width: rects['first'].width,
+    height: rects['first'].height
+  },
+  'second': {
+    x: rects['second'].x,
+    y: rects['second'].y,
+    width: rects['second'].width,
+    height: rects['second'].height
+  }
 });
-var server = app.listen(3000, function () {
+});
 
+var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
-
 });
