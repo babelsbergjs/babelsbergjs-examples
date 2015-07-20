89bcf430dd616a3d1fecad480d4996c56f5d054e
FHorschig <f.horschig@googlemail.com>
2015-07-20 17:25:31 +0200

    Added uglify.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,17 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    uglify: {
+      dist: {
+        files: {
+          'dist/output.min.js': ['tmp/**/*.js']
+        }
+      }
+    },
     watch: {
       scripts: {
         files: ['server/**/*.bbb'],
-        tasks: ['babelsbergjs'],
+        tasks: ['babelsbergjs', 'uglify'],
         options: {
           spawn: false,
         },
@@ -14,14 +21,15 @@ module.exports = function(grunt) {
       build: {
         cwd: 'server/',
         src: '**/*.bbb',
-        dest: 'dist/'
+        dest: 'tmp/'
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-babelsbergjs');
 
-  grunt.registerTask('default', ['babelsbergjs']);
+  grunt.registerTask('default', ['babelsbergjs', 'uglify']);
 
 };
